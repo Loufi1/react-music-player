365e5ebbbcf233597aa7db2e5a2d2d131dce73c2
Louis-Frédéric Fortier <loufi@mbpdelofrederic.home>
2020-11-22 00:58:37 +0100

    Create layout

--- src/components/player.js
+++ src/components/player.js
@@ -1,11 +1,24 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {
+    faPlayCircle,
+    faStepForward,
+    faStepBackward,
+} from '@fortawesome/free-solid-svg-icons'
 
 const Player = () => {
     return (
         <div className="player">
-            <p>0.0</p>
-            <input type='range'/>
-            <p>1.26</p>
+            <div className="input">
+                <p>0.0</p>
+                <input type='range'/>
+                <p>1.26</p>
+            </div>
+            <div className="control">
+                <FontAwesomeIcon size='2x' icon={faStepBackward} />
+                <FontAwesomeIcon size='2x' icon={faPlayCircle} />
+                <FontAwesomeIcon size='2x' icon={faStepForward} />
+            </div>
         </div>
     )
 }
