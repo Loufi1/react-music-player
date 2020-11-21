394edabe4b7497a0c5c6ea008f81e2d9614ab3bf
Louis-Frédéric Fortier <loufi@mbpdelofrederic.home>
2020-11-21 20:21:25 +0100

    Center player

--- src/components/player.js
+++ src/components/player.js
@@ -3,7 +3,9 @@ import React from "react";
 const Player = () => {
     return (
         <div className="player">
+            <p>0.0</p>
             <input type='range'/>
+            <p>1.26</p>
         </div>
     )
 }
