2d320f906acdb73453c2a8a2145395407050ea26
Louis-Frédéric Fortier <loufi@mbpdelofrederic.home>
2020-11-22 14:16:54 +0100

    Add music file

---
+++ src/utils/musics.js
@@ -0,0 +1,24 @@
+const getMusics = () => {
+    return(
+        [
+            {
+                id: 1,
+                name: 'CORS',
+                artist: 'Loufi',
+                url: 'localhost:8000/Loufi-CORS.wav',
+                coverUrl: 'https://i1.sndcdn.com/artworks-zTMazAz5P23zRzIT-GxJlrA-t500x500.jpg',
+                colors: ['#111111', '#D81212'],
+                active: false,
+            },
+            {
+                id: 2,
+                name: 'Feeling Good',
+                artist: 'Loufi',
+                url: 'localhost:8000/Loufi-Feeling_Good.wav',
+                coverUrl: 'https://i1.sndcdn.com/artworks-4noRuznX3eS1bP6q-FRvdAg-t500x500.jpg',
+                colors: ['#443665', '#cd91ae', '#647de3', '#9c6c94'],
+                active: false,
+            }
+        ]
+    )
+}
