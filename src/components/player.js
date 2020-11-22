89a7b47c052b71816cdb20592c91f8dea59f23e6
Louis-Frédéric Fortier <loufi@mbpdelofrederic.home>
2020-11-22 21:01:26 +0100

    Can move player's cursor

--- src/components/player.js
+++ src/components/player.js
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
     faPlayCircle,
@@ -6,9 +6,12 @@ import {
     faStepForward,
     faStepBackward,
 } from '@fortawesome/free-solid-svg-icons'
+import getTime from "../utils/formatTime";
 
 const Player = ({playingSong, isPlaying, setIsPlaying}) => {
     const audioRef = useRef(null);
+    const [elapsedTime, setElapsedTime] = useState(0);
+    const [duration, setDuration] = useState(0);
 
     const play = () => {
         if (isPlaying) {
@@ -20,19 +23,31 @@ const Player = ({playingSong, isPlaying, setIsPlaying}) => {
         }
     }
 
+    const updateTime = (e) => {
+        setDuration(e.target.duration);
+        setElapsedTime(e.target.currentTime);
+    }
+
+    const updateCursor = (e) => {
+        audioRef.current.currentTime = e.target.value;
+        console.log(e.target.value) // equal 15
+        console.log(audioRef.current.currentTime) // equal 0
+        setElapsedTime(e.target.value);
+    }
+
     return (
         <div className="player">
             <div className="input">
-                <p>0.0</p>
-                <input type='range'/>
-                <p>1.26</p>
+                <p>{getTime(elapsedTime)}</p>
+                <input onChange={updateCursor} min={0} max={duration} value={elapsedTime} type='range'/>
+                <p>{getTime(duration)}</p>
             </div>
             <div className="control">
                 <FontAwesomeIcon size='2x' icon={faStepBackward} />
                 <FontAwesomeIcon onClick={play} size='2x' icon={isPlaying ? faPauseCircle : faPlayCircle} />
                 <FontAwesomeIcon size='2x' icon={faStepForward} />
             </div>
-            <audio ref={audioRef} src={playingSong.url}/>
+            <audio onLoadedMetadata={updateTime} onTimeUpdate={updateTime} ref={audioRef} src={playingSong.url}/>
         </div>
     )
 }
